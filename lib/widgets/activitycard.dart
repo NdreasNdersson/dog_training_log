c5fe49e448c31fc0b81a90fbdf0d7d8e6a67f3cf
Andreas Andersson <e.andreas.asson@gmail.com>
2022-03-07 20:25:11 +0100

    Width fixed

--- lib/widgets/activitycard.dart
+++ lib/widgets/activitycard.dart
@@ -44,61 +44,79 @@ class _ActivityCard extends State<ActivityCard> {
                   children: [
                     TextField(
                       controller: _typeController,
+                      decoration: const InputDecoration(
+                          disabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          labelText: 'Type',
+                          contentPadding: EdgeInsets.all(5)),
                     ),
                     TextField(
                       controller: _distanceController,
                       keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                          disabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          labelText: 'Distance',
+                          contentPadding: EdgeInsets.all(5)),
                     ),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //   children: [
-                    InkWell(
-                      onTap: () {
-                        _selectDate(context);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 30),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(color: Colors.grey[200]),
-                        child: TextFormField(
-                          style: const TextStyle(fontSize: 40),
-                          textAlign: TextAlign.center,
-                          enabled: false,
-                          keyboardType: TextInputType.text,
-                          controller: _dateController,
-                          decoration: const InputDecoration(
-                              disabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide.none),
-                              contentPadding: EdgeInsets.only(top: 0.0)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            _selectDate(context);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 30),
+                            alignment: Alignment.center,
+                            width: 150,
+                            decoration: BoxDecoration(color: Colors.grey[200]),
+                            child: TextFormField(
+                              style: const TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                              enabled: false,
+                              keyboardType: TextInputType.text,
+                              controller: _dateController,
+                              decoration: const InputDecoration(
+                                  disabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide.none),
+                                  labelText: 'Date',
+                                  contentPadding: EdgeInsets.all(5)),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        _selectTime(context);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 30),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(color: Colors.grey[200]),
-                        child: TextFormField(
-                          style: const TextStyle(fontSize: 40),
-                          textAlign: TextAlign.center,
-                          enabled: false,
-                          keyboardType: TextInputType.text,
-                          controller: _timeController,
-                          decoration: const InputDecoration(
-                              disabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide.none),
-                              // labelText: 'Time',
-                              contentPadding: EdgeInsets.all(5)),
+                        InkWell(
+                          onTap: () {
+                            _selectTime(context);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 30),
+                            alignment: Alignment.center,
+                            width: 100,
+                            decoration: BoxDecoration(color: Colors.grey[200]),
+                            child: TextFormField(
+                              style: const TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                              enabled: false,
+                              keyboardType: TextInputType.text,
+                              controller: _timeController,
+                              decoration: const InputDecoration(
+                                  disabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide.none),
+                                  labelText: 'Time',
+                                  contentPadding: EdgeInsets.all(5)),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                    //   ],
-                    // ),
                     TextField(
                       controller: _commentController,
+                      decoration: const InputDecoration(
+                          disabledBorder:
+                              UnderlineInputBorder(borderSide: BorderSide.none),
+                          labelText: 'Comment',
+                          contentPadding: EdgeInsets.all(5)),
                     ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
