fb9e9316f31fe0051a5f7598ef1add1f2bfed156
Andreas Andersson <e.andreas.asson@gmail.com>
2022-04-03 18:03:23 +0200

    Added map

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
 import 'package:dog_training_log/models/dog.dart';
 import 'package:dog_training_log/pages/activitylistpage.dart';
 import 'package:dog_training_log/widgets/bottombar.dart';
 import 'package:dog_training_log/widgets/headerbar.dart';
 import 'package:dog_training_log/models/activity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
@@ -16,57 +20,77 @@ Future<void> main() async {
   Hive.registerAdapter(DogAdapter());
   await Hive.openBox<Dog>('dogs');
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Dog training log',
-        theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-        ),
-        home: const ActivityListPage() //const MyHomePage(),
-        );
+      title: 'Dog training log',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
-  final String title = 'Home';
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final String _text = "No activity";
+  Completer<GoogleMapController> _controller = Completer();
 
-  _MyHomePageState();
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(17.385044, 78.486671),
+    zoom: 18,
+  );
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  late MarkerId markerId1;
+  late Marker marker1;
+
+  @override
+  void initState() {
+    super.initState();
+    markerId1 = MarkerId("Current");
+    marker1 = Marker(
+        markerId: markerId1,
+        position: LatLng(17.385044, 78.486671),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        infoWindow: InfoWindow(
+            title: "Hytech City", onTap: () {}, snippet: "Snipet Hitech City"));
+    markers[markerId1] = marker1;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: const HeaderBar('Home'),
-        body: Stack(children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                'Last activity:',
-              ),
-              Text(
-                _text,
-                style: Theme.of(context).textTheme.headline4,
+    return new Scaffold(
+        drawer: const Drawer(),
+        appBar: const HeaderBar('Map view'),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Stack(
+            children: [
+              GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+                markers: Set<Marker>.of(markers.values),
               ),
             ],
-          )
-        ]),
+          ),
+        ),
         bottomNavigationBar: const BottomBar());
   }
 }
